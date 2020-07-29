247c6a23ff01f03689e40956c39cfc61229ff3c8
Zaid Abdulla <zaid@genieacs.com>
2020-07-29 01:23:20 -0700

    Use mjs and cjs extensions

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,34 @@ import { resolve, dirname } from "path";
 import typescript from '@rollup/plugin-typescript';
 
 export default [
-	// Native BigInt
-	{
+  // Native BigInt
+  {
     input: "src/main.ts",
-		plugins: [
+    plugins: [
       typescript()
-		],
-		output: [
-      { file: "dist/yebool.cjs.js", format: "cjs" },
-			{ file: "dist/yebool.esm.js", format: 'es' }
-		]
+    ],
+    output: [
+      { file: "dist/yebool.cjs", format: "cjs" },
+      { file: "dist/yebool.mjs", format: 'es' }
+    ]
   },
   
   // JSBI
   {
     input: 'src/main.ts',
     external: ["jsbi"],
-		plugins: [
-			{
-				resolveId: (importee, importer) => {
-				  if (importee.endsWith("/bigint")) return resolve(dirname(importer), "bigint-jsbi.ts");
-				  return null;
-				}
-			},	  
-			typescript()
-		],
-		output: [
-			{ file: "dist/yebool-jsbi.cjs.js", format: "cjs" },
-			{ file: "dist/yebool-jsbi.esm.js", format: 'es' }
-		]
-	}	
+    plugins: [
+      {
+        resolveId: (importee, importer) => {
+          if (importee.endsWith("/bigint")) return resolve(dirname(importer), "bigint-jsbi.ts");
+          return null;
+        }
+      },	  
+      typescript()
+    ],
+    output: [
+      { file: "dist/yebool-jsbi.cjs", format: "cjs" },
+      { file: "dist/yebool-jsbi.mjs", format: 'es' }
+    ]
+  }	
 ];
