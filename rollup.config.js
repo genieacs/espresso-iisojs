a24adf9aa7885302573b5a5ceb4b5bbc9bace8a5
Zaid Abdulla <zaid@genieacs.com>
2021-09-03 01:44:11 -0700

    Remove JSBI dependency
    BigInt is now supported in all major browsers!

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,12 @@
-import { resolve, dirname } from "path";
 import typescript from '@rollup/plugin-typescript';
 
-export default [
-  // Native BigInt
-  {
-    input: "src/main.ts",
-    plugins: [
-      typescript()
-    ],
-    output: [
-      { file: "dist/espresso-iisojs.cjs", format: "cjs" },
-      { file: "dist/espresso-iisojs.mjs", format: 'es' }
-    ]
-  },
-  
-  // JSBI
-  {
-    input: 'src/main.ts',
-    external: ["jsbi"],
-    plugins: [
-      {
-        resolveId: (importee, importer) => {
-          if (importee.endsWith("/bigint")) return resolve(dirname(importer), "bigint-jsbi.ts");
-          return null;
-        }
-      },	  
-      typescript()
-    ],
-    output: [
-      { file: "dist/espresso-iisojs-jsbi.cjs", format: "cjs" },
-      { file: "dist/espresso-iisojs-jsbi.mjs", format: 'es' }
-    ]
-  }	
-];
+export default {
+  input: "src/main.ts",
+  plugins: [
+    typescript()
+  ],
+  output: [
+    { file: "index.mjs", format: 'es' },
+    { file: "index.cjs", format: "cjs" }
+  ]
+};
